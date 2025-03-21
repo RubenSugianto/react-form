e8eb0c04ecea0319fb61ad97c8919d061e92ff2c
RubenSugianto <ruben.sugianto@gmail.com>
2025-03-21 23:20:30 +0700

    Handling Form Submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,15 @@
 export default function Login() {
-  return (
-    <form>
-      <h2>Login</h2>
+  
 
+  function handleSubmit(event) {
+    event.preventDefault(); // ini agar form submit nggak refresh page
+    console.log('Submitted');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      {/* HTML FOR tuh kayak for di HTML biasa */}
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
@@ -17,7 +24,11 @@ export default function Login() {
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
+        {/* button type ada 3 : reset, submit,  reset (by default itu submit)*/}
+        {/* kalo submit, dia tuh bakal otomatis refresh page karna kirim data ke server react */}
+        {/* pada umumnya ya kita pasti maunya ngirim data ke API Eksternal */}
         <button className="button">Login</button>
+        {/* <button type="button" className="button" onClick={handleSubmit}>Login</button> */}
       </p>
     </form>
   );
